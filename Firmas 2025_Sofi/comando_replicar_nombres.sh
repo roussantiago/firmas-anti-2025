b082472d75957c543a7ad38ff032f853c23ad595
roussantiago <144722478+roussantiago@users.noreply.github.com>
2025-02-18 18:07:18 +0100

    Add files via upload

---
+++ Firmas 2025_Sofi/comando_replicar_nombres.sh
@@ -0,0 +1,16 @@
+#Modificación de firmas
+#Código para versión adaptada a móvil, solo por el tema del +
+
+nombre="Sofía Bodoque"
+puesto="Producer"
+numero="+34699973585" #ej.: +34666666666
+numero_vista="+34 699.973.585" #ej.: +34 666.666.666
+
+#comando
+sed -i '' "s/Nombre/$nombre/g" firma_avion_v2.html firma_cereza_v2.html firma_claqueta_v2.html firma_coche_v2.html firma_skater_v2.html
+sed -i '' "s/Puesto/$puesto/g" firma_avion_v2.html firma_cereza_v2.html firma_claqueta_v2.html firma_coche_v2.html firma_skater_v2.html
+sed -i '' "s/numero/$numero/g" firma_avion_v2.html firma_cereza_v2.html firma_claqueta_v2.html firma_coche_v2.html firma_skater_v2.html
+sed -i '' "s/nu.me.ro/$numero_vista/g" firma_avion_v2.html firma_cereza_v2.html firma_claqueta_v2.html firma_coche_v2.html firma_skater_v2.html
+
+#recordatorios
+echo "Bueno pues ahora te llamas $nombre, eres $puesto, tu numero se ve como $numero_vista, y si te llaman, llaman a $numero"
